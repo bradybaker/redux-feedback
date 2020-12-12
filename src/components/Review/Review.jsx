7f9b312d2e46f4bbd2a88ae5b735a224aaa64abd
Brady Baker <bradybaker82@gmail.com>
2020-12-11 23:37:36 -0600

    Reponses are all on the review page, will post to database next

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -4,6 +4,15 @@ import { connect } from 'react-redux'
 
 class Review extends Component {
 
+    state = {
+        response: {
+            feeling: this.props.feelingReducer.feeling,
+            understanding: this.props.understandingReducer.understanding,
+            support: this.props.supportReducer.support,
+            comments: this.props.commentReducer.comments
+        }
+    }
+
     handleSubmit = (event) => {
         event.preventDefault()
         this.props.history.push('/thankyou')
@@ -13,12 +22,13 @@ class Review extends Component {
         const { feelingReducer, understandingReducer, supportReducer, commentReducer } = this.props
         return (
             <div>
+                {JSON.stringify(this.state.response)}
                 <h1>Review Responses</h1>
                 <form onSubmit={this.handleSubmit}>
                     <p>Feeling: {feelingReducer.feeling}  </p>
                     <p>Understanding: {understandingReducer.understanding}</p>
-                    <p>Support: </p>
-                    <p>Comments: </p>
+                    <p>Support: {supportReducer.support}</p>
+                    <p>Comments: {commentReducer.comments}</p>
                     <button type='submit'>Submit Feedback</button>
                 </form>
 
